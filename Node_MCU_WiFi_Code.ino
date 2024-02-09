8da20dda51463921e9f69b6be3cf0490b97df631
Gaurav Kumar <62889542+gauravk5@users.noreply.github.com>
2024-02-10 01:24:14 +0530

    Add files via upload

---
+++ Node_MCU_WiFi_Code.ino
@@ -0,0 +1,29 @@
+/**************
+
+Techeonics
+esp8266 connect to wifi 
+By Gaurav Kumar (Techeonics) 
+<thetecheonics@gmail.com>
+Youtube- https://www.youtube.com/c/THETECHEONICS
+See <http://www.techeonics.com> for details.
+
+********************/
+#include <ESP8266WiFi.h>
+const char* ssid = "xxxxxxxxxx"; // Name of the Network.
+const char* password = "xxxxxxxxxx"; // Password of the Network.
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, password); // This will connect the Node MCU with WiFi.
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP()); //This will print the Local IP address of the Network.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
